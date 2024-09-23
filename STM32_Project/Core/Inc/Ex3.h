4cbb06544c5c1f1608089865434f4c1420bce6b5
txphu2302 <phu.tran232@hcmut.edu.vn>
2024-09-23 12:21:40 +0700

    Init for Ex3

---
+++ STM32_Project/Core/Inc/Ex3.h
@@ -0,0 +1,17 @@
+/*
+ * Ex3.h
+ *
+ *  Created on: Sep 23, 2024
+ *      Author: Acer
+ */
+
+#ifndef INC_EX3_H_
+#define INC_EX3_H_
+
+#include "main.h"
+
+extern int num;
+void ex3();
+
+
+#endif /* INC_EX3_H_ */
--- STM32_Project/Core/Src/main.c
+++ STM32_Project/Core/Src/main.c
@@ -22,6 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include "Ex3.h"
 
 /* USER CODE END Includes */
 
@@ -94,7 +95,8 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+	  //TODO Ex3
+	  ex3();
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
